d929f15ccd7c585533f9a0361d6910a0ff7e25b6
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2021-05-16 22:05:18 +0900

    footer done

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import Cards from '../Cards';
+import Footer from '../Footer';
 import HeroSection from '../HeroSection';
 
 function Home(props) {
@@ -8,6 +9,7 @@ function Home(props) {
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 }
---
+++ src/components/pages/Services.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+function Services(props) {
+  return (
+    <h1 className='services'>SERVICES</h1>
+  );
+}
+
+export default Services;
