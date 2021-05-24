1997be06ad56ef5997b574dc5dfd9c59fe1144f6
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2021-05-24 18:18:03 +0900

    import files

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+import Img9 from '../images/img-9.jpg'
+import Img2 from '../images/img-2.jpg'
+import Img3 from '../images/img-3.jpg'
+import Img4 from '../images/img-4.jpg'
+import Img8 from '../images/img-8.jpg'
 
 function Cards(props) {
   return (
@@ -10,13 +15,13 @@ function Cards(props) {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem 
-              src="../../images/img-9.jpg"  
+              src={Img9}
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem 
-              src="../../images/img-2.jpg"  
+              src={Img2}  
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               path="/services"
@@ -24,21 +29,19 @@ function Cards(props) {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='../../images/img-3.jpg'
+              src={Img3}
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='../../images/img-4.jpg'
+              src={Img4}
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
-
-              
               path='/products'
             />
             <CardItem
-              src='../../images/img-8.jpg'
+              src={Img8}
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
